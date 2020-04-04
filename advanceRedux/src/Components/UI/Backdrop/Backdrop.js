9bd435479b013af49ad42e05d0af0fe2dbe0b00d
aasthadawar <aastha.dawar@tothenew.com>
2020-04-04 14:11:36 +0530

    delete

--- advanceRedux/src/Components/UI/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from '../Backdrop/Backdrop.module.css';
-
-
-const Backdrop=(props)=>(
-    props.show ? <div onClick={props.clicked} className={styles.Backdrop}></div> : null
-)
-
-export default Backdrop;
